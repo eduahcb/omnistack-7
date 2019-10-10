906515d920620e2090dcbca4eaaf0d70a73e763e
parzivalEduardo <dev_eduardo@hotmail.com>
2019-10-09 21:31:57 -0300

    levando projeto mobile.

---
+++ mobile/src/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+import Routes from './routes';
+
+// import { Container } from './styles';
+
+export default class App extends Component {
+  render() {
+    return <Routes />;
+  }
+}
